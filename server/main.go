50e767c14a062ea76e6f255a30f79d022083315e
Max Garvey <maxgarvey@gmail.com>
2015-09-29 13:03:18 -0700

    stub out api responses.

--- server/main.go
+++ server/main.go
@@ -27,8 +27,15 @@ func New() Instance {
 func (instance *Instance) Initialize() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/status", StatusHandler)
-	router.HandleFunc("/api", ApiHandler)
+	apiRouter := router.PathPrefix("/api").Subrouter()
+	apiRouter.PathPrefix("/configurations").HandlerFunc(
+		ConfigurationsHandler)
+	apiRouter.PathPrefix("/configuration/{configurationId}").HandlerFunc(
+		GetPutConfigurationHandler)
+	apiRouter.PathPrefix("/configuration").HandlerFunc(
+		NewConfigurationHandler)
+
+	router.PathPrefix("/status").HandlerFunc(StatusHandler)
 	router.PathPrefix("/static").Handler(
 		http.StripPrefix("/static",
 			http.FileServer(
@@ -57,9 +64,35 @@ func StatusHandler(respW http.ResponseWriter, req *http.Request) {
 	io.WriteString(respW, "status OK\n")
 }
 
-func ApiHandler(respW http.ResponseWriter, req *http.Request) {
-	fmt.Printf("in api handler.\n") // debug
+func NewApiRouter() http.Handler {
+	fmt.Printf("in NewApiRouter().")
+	router := mux.NewRouter()
 
-	respW.WriteHeader(http.StatusOK)
-	io.WriteString(respW, "api response here\n")
+	router.HandleFunc("/api/configurations", ConfigurationsHandler)
+
+	return router
+}
+
+func ConfigurationsHandler(respW http.ResponseWriter, req *http.Request) {
+	fmt.Printf("in configurations handler.\n") // debug
+
+	// respW.WriteHeader(http.StatusOK)
+	// io.WriteString(respW, "api response here\n")
+
+	respW.WriteHeader(http.StatusOK)                            // debug
+	io.WriteString(respW, "configurations api response here\n") // debug
+}
+
+func NewConfigurationHandler(respW http.ResponseWriter, req *http.Request) {
+	fmt.Printf("in new configuration handler.\n")
+
+	respW.WriteHeader(http.StatusOK)                                // debug
+	io.WriteString(respW, "new configuration api response here.\n") // debug
+}
+
+func GetPutConfigurationHandler(respW http.ResponseWriter, req *http.Request) {
+	fmt.Printf("in GET/PUT configuration handler.\n")
+
+	respW.WriteHeader(http.StatusOK)                               // debug
+	io.WriteString(respW, "get put configuration handler here.\n") // debug
 }
